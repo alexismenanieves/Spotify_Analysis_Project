bff391336432b5706a3ca8a19cd271547c8a9c11
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-26 22:23:40 -0500

    Corrected code on API call

--- Spotify_Analysis_Connection.R
+++ Spotify_Analysis_Connection.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(purrr)
 library(jsonlite)
 library(glue)
+library(lubridate)
 
 # This function retrieves the access token from Spotify via its API
 get_spotify_access <- function(
@@ -29,9 +30,10 @@ get_spotify_access <- function(
 
 # This is the main function that retrieves artist, albums and tracks
 get_artist_audio_features <- function(
-  artist = NULL, include_groups = 'albums', 
+  artist = NULL, include_groups = 'album', 
   return_closest_artist = TRUE, dedupe_albums = TRUE,
   authorization = get_spotify_access()) {
+  
   if(is_uri(artist)){
     artist_info = get_artist(
       artist, authorization = get_spotify_access)
@@ -39,14 +41,16 @@ get_artist_audio_features <- function(
     artist_name <- artist_id$name
   } else {
     artist_ids <- search_spotify(
-      artist, "artist", autorizathion = authorization)
+      artist, "artist", authorization = authorization)
+    
     if(return_closest_artist){
-      artist_id <- artists_ids$id[1]
-      artist_name <- artists_ids$name[1]
+      artist_id <- artist_ids$id[1]
+      artist_name <- artist_ids$name[1]
     } else {
       choices <- map_char(1:length(artist_ids$name), function(x) {
         glue("[{x}] {artist_ids$name[x]}")
       }) %>% paste0(collapse = '\n\t')
+      
       cat(glue("Your artist {artist} could be in this list",
                "\nSelect the number"))
       selection <- as.numeric(readline())
@@ -58,46 +62,46 @@ get_artist_audio_features <- function(
   artist_albums <- get_artist_albums(
     artist_id, include_groups = include_groups,
     include_meta_info = TRUE, authorization = authorization)
-  
+
   num_loops_albums <- ceiling(artist_albums$total / 20)
   if(num_loops_albums > 1) {
     artist_albums <- map_df(1:num_loops_albums, function(x){
       get_artist_albums(
         artist_id, include_groups = include_groups,
-        include_meta_info = TRUE, autorization = autorization,
-        offset = (x-1) * 20)
+        offset = (x-1) * 20, authorization = authorization)
     })
   } else {
     artist_albums <- artist_albums$items
   }
-  
+
   artist_albums <- artist_albums %>%
     rename(album_id = id,
            album_name = name) %>%
     mutate(album_release_year = case_when(
-      release_date_precision = "year" ~ 
+      release_date_precision == "year" ~
         suppressWarnings(as.numeric(release_date)),
-      release_date_precision = "day" ~
+      release_date_precision == "day" ~
         year(as.Date(release_date,"%Y-%m-%d",origin = "1970-01-01")),
       TRUE ~ as.numeric(NA)
     ))
   
   album_tracks <- map_df(artist_albums$album_id, function(x) {
-    album_tracks <- get_album_tracks(x, include_meta_info = TRUE, 
+    print(x)
+    album_tracks <- get_album_tracks(x, include_meta_info = TRUE,
                                      authorization = authorization)
     num_loops_tracks <- ceiling(album_tracks$total / 20)
     if(num_loops_tracks > 1) {
       album_tracks <- map_df(1:num_loops_tracks, function(y){
-        get_album_tracks(x, offset = (y -1)*20, 
+        get_album_tracks(x, offset = (y -1)*20,
                          authorization = authorization)
       })
     } else {
       album_tracks <- album_tracks$items
     }
-    
+
     album_tracks <- album_tracks %>%
       mutate(album_id = x,
-             album_name = 
+             album_name =
                artist_albums$album_name[artist_albums$album_id == x]) %>%
       rename(track_name = name,
              track_uri = uri,
@@ -105,26 +109,27 @@ get_artist_audio_features <- function(
              track_href = href,
              track_id = id)
   })
-  
+
   total_loop_tracks <- ceiling(nrow(album_tracks)/100)
   track_audio_features <- map_df(
     1:total_loop_tracks, function(x){
-      tracks_id <- album_tracks %>%
+      track_ids <- album_tracks %>%
         slice(((x*100)-99):(x*100)) %>%
         pull(track_id)
+      get_track_audio_features(track_ids, authorization = authorization)
     }
-  ) %>% 
+  ) %>%
     select(-c("duration_ms","type","uri","track_href")) %>%
     rename(track_id = id) %>%
     left_join(album_tracks, by = "track_id")
-  
+
   artist_albums %>%
     mutate(artist_name = artist_name,
            artist_id = artist_id) %>%
-    select(artist_name, artist_id, album_id, 
-           album_type, album_images = images, 
+    select(artist_name, artist_id, album_id,
+           album_type, album_images = images,
            album_release_date = release_date,
-           release_year, 
+           release_year,
            album_release_date_precision = release_date_precision) %>%
     left_join(track_audio_features, by = "album_id") %>%
     mutate(key_name = pitch_class_lookup[key + 1],
@@ -185,11 +190,11 @@ get_artist_albums <- function(
 }
 
 get_album_tracks <- function(
-  id, limit = 20, offset = 20, market = NULL,
+  id, limit = 20, offset = 0, market = NULL,
   authorization = get_spotify_access(),
   include_meta_info = FALSE) {
   
-  base_url <- 'https://api.spotify.com/v1/artists'
+  base_url <- 'https://api.spotify.com/v1/albums'
   
   if(!is.null(market)){
     if(!grepl('^[[:alpha:]]{2}$',market)){
@@ -214,6 +219,26 @@ get_album_tracks <- function(
   if(!include_meta_info){
     res <- res$items
   }
+  
+  return(res)
+}
+
+get_track_audio_features <- function(
+  ids, authorization = get_spotify_access()) {
+  
+  stopifnot(length(ids)<=100)
+  base_url <- "https://api.spotify.com/v1/audio-features"
+  params <- list(
+    access_token = authorization,
+    ids = paste0(ids, collapse = ",")
+  )
+  
+  res <- RETRY("GET", base_url, query = params, encode = "json")
+  stop_for_status(res)
+  res <- fromJSON(content(res, as = "text", encoding = "utf-8"),
+                  flatten = TRUE) %>%
+    .$audio_features %>%
+    as_tibble()
   
   return(res)
 }
