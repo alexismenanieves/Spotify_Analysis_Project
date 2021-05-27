0384fd97b2258359debcdb817da30f39f32b6ab5
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-26 22:54:34 -0500

    Start loading data and exploring

--- Spotify_Analysis_Script.R
+++ Spotify_Analysis_Script.R
@@ -1,4 +1,21 @@
-source("Spotify_Analysis_Connection.R")
-hector_songs <- get_artist_audio_features("hector lavoe")
+# ------------------------------------------------------------------------#
+# Author: Manuel Mena
+# Date: 26/05/2021
+# Project: Spotify Analysis
+# Scope: Feature analysis
+# ------------------------------------------------------------------------#
 
+# Step 0. Load libraries and functions ------------------------------------
+library(dplyr)
+library(ggplot2)
+library(ggtext)
+library(ggdendro)
+
+source("Spotify_Analysis_Connection.R")
+
+# Step 1. Load data and make basic exploration ----------------------------
 pearl_jam_songs <- get_artist_audio_features("pearl jam")
+dim(pearl_jam_songs)
+str(pearl_jam_songs)
+
+
